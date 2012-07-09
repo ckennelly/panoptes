d55fa608f2a079be0608db08cfc9bef005e1a2d7
Chris Kennelly <ckennelly@ckennelly.com>
2012-07-08 22:49:33 -0400

    Test other forms of testp.

--- src/tests/vtest_k_testp.cu
+++ src/tests/vtest_k_testp.cu
@@ -17,8 +17,90 @@
  */
 
 #include <boost/static_assert.hpp>
-#include <cuda.h>
 #include <gtest/gtest.h>
+#include <stdint.h>
+#include <valgrind/memcheck.h>
+
+template<typename T>
+__global__ void k_isfinite(bool * d, const T a) {
+    BOOST_STATIC_ASSERT(sizeof(T) == 0);
+}
+
+template<>
+__global__ void k_isfinite(bool * d, const float a) {
+    int ret;
+    asm volatile(
+        "{ .reg .pred %tmp;\n"
+        "  testp.finite.f32 %tmp, %1;\n"
+        "  selp.b32 %0, 1, 0, %tmp; }\n" :
+        "=r"(ret) : "f"(a));
+    *d = ret;
+}
+
+template<>
+__global__ void k_isfinite(bool * d, const double a) {
+    int ret;
+    asm volatile(
+        "{ .reg .pred %tmp;\n"
+        "  testp.finite.f64 %tmp, %1;\n"
+        "  selp.b32 %0, 1, 0, %tmp; }\n" :
+        "=r"(ret) : "d"(a));
+    *d = ret;
+}
+
+template<typename T>
+__global__ void k_isinfinite(bool * d, const T a) {
+    BOOST_STATIC_ASSERT(sizeof(T) == 0);
+}
+
+template<>
+__global__ void k_isinfinite(bool * d, const float a) {
+    int ret;
+    asm volatile(
+        "{ .reg .pred %tmp;\n"
+        "  testp.infinite.f32 %tmp, %1;\n"
+        "  selp.b32 %0, 1, 0, %tmp; }\n" :
+        "=r"(ret) : "f"(a));
+    *d = ret;
+}
+
+template<>
+__global__ void k_isinfinite(bool * d, const double a) {
+    int ret;
+    asm volatile(
+        "{ .reg .pred %tmp;\n"
+        "  testp.infinite.f64 %tmp, %1;\n"
+        "  selp.b32 %0, 1, 0, %tmp; }\n" :
+        "=r"(ret) : "d"(a));
+    *d = ret;
+}
+
+template<typename T>
+__global__ void k_isnumber(bool * d, const T a) {
+    BOOST_STATIC_ASSERT(sizeof(T) == 0);
+}
+
+template<>
+__global__ void k_isnumber(bool * d, const float a) {
+    int ret;
+    asm volatile(
+        "{ .reg .pred %tmp;\n"
+        "  testp.number.f32 %tmp, %1;\n"
+        "  selp.b32 %0, 1, 0, %tmp; }\n" :
+        "=r"(ret) : "f"(a));
+    *d = ret;
+}
+
+template<>
+__global__ void k_isnumber(bool * d, const double a) {
+    int ret;
+    asm volatile(
+        "{ .reg .pred %tmp;\n"
+        "  testp.number.f64 %tmp, %1;\n"
+        "  selp.b32 %0, 1, 0, %tmp; }\n" :
+        "=r"(ret) : "d"(a));
+    *d = ret;
+}
 
 template<typename T>
 __global__ void k_isnan(bool * d, const T a) {
@@ -47,12 +129,66 @@ __global__ void k_isnan(bool * d, const double a) {
     *d = ret;
 }
 
-TEST(TestPTest, SingleIsNan) {
+template<typename T>
+__global__ void k_isnormal(bool * d, const T a) {
+    BOOST_STATIC_ASSERT(sizeof(T) == 0);
+}
+
+template<>
+__global__ void k_isnormal(bool * d, const float a) {
+    int ret;
+    asm volatile(
+        "{ .reg .pred %tmp;\n"
+        "  testp.normal.f32 %tmp, %1;\n"
+        "  selp.b32 %0, 1, 0, %tmp; }\n" :
+        "=r"(ret) : "f"(a));
+    *d = ret;
+}
+
+template<>
+__global__ void k_isnormal(bool * d, const double a) {
+    int ret;
+    asm volatile(
+        "{ .reg .pred %tmp;\n"
+        "  testp.normal.f64 %tmp, %1;\n"
+        "  selp.b32 %0, 1, 0, %tmp; }\n" :
+        "=r"(ret) : "d"(a));
+    *d = ret;
+}
+
+template<typename T>
+__global__ void k_issubnormal(bool * d, const T a) {
+    BOOST_STATIC_ASSERT(sizeof(T) == 0);
+}
+
+template<>
+__global__ void k_issubnormal(bool * d, const float a) {
+    int ret;
+    asm volatile(
+        "{ .reg .pred %tmp;\n"
+        "  testp.subnormal.f32 %tmp, %1;\n"
+        "  selp.b32 %0, 1, 0, %tmp; }\n" :
+        "=r"(ret) : "f"(a));
+    *d = ret;
+}
+
+template<>
+__global__ void k_issubnormal(bool * d, const double a) {
+    int ret;
+    asm volatile(
+        "{ .reg .pred %tmp;\n"
+        "  testp.subnormal.f64 %tmp, %1;\n"
+        "  selp.b32 %0, 1, 0, %tmp; }\n" :
+        "=r"(ret) : "d"(a));
+    *d = ret;
+}
+
+TEST(TestPTest, Single) {
     cudaError_t ret;
     cudaStream_t stream;
 
     bool * d;
-    ret = cudaMalloc((void **) &d, 2 * sizeof(d));
+    ret = cudaMalloc((void **) &d, 12 * sizeof(d));
     ASSERT_EQ(cudaSuccess, ret);
 
     ret = cudaStreamCreate(&stream);
@@ -60,8 +196,23 @@ TEST(TestPTest, SingleIsNan) {
 
     const float  fs = 0.5f;
     const double fd = 0.2;
-    k_isnan<<<1, 1, 0, stream>>>(d + 0, fs);
-    k_isnan<<<1, 1, 0, stream>>>(d + 1, fd);
+    k_isfinite   <<<1, 1, 0, stream>>>(d + 0,  fs);
+    k_isfinite   <<<1, 1, 0, stream>>>(d + 1,  fd);
+
+    k_isinfinite <<<1, 1, 0, stream>>>(d + 2,  fs);
+    k_isinfinite <<<1, 1, 0, stream>>>(d + 3,  fd);
+
+    k_isnumber   <<<1, 1, 0, stream>>>(d + 4,  fs);
+    k_isnumber   <<<1, 1, 0, stream>>>(d + 5,  fd);
+
+    k_isnan      <<<1, 1, 0, stream>>>(d + 6,  fs);
+    k_isnan      <<<1, 1, 0, stream>>>(d + 7,  fd);
+
+    k_isnormal   <<<1, 1, 0, stream>>>(d + 8,  fs);
+    k_isnormal   <<<1, 1, 0, stream>>>(d + 9,  fd);
+
+    k_issubnormal<<<1, 1, 0, stream>>>(d + 10, fs);
+    k_issubnormal<<<1, 1, 0, stream>>>(d + 11, fd);
 
     ret = cudaStreamSynchronize(stream);
     ASSERT_EQ(cudaSuccess, ret);
@@ -69,12 +220,22 @@ TEST(TestPTest, SingleIsNan) {
     ret = cudaStreamDestroy(stream);
     ASSERT_EQ(cudaSuccess, ret);
 
-    bool hd[2];
+    bool hd[12];
     ret = cudaMemcpy(&hd, d, sizeof(hd), cudaMemcpyDeviceToHost);
     ASSERT_EQ(cudaSuccess, ret);
 
-    EXPECT_FALSE(hd[0]);
-    EXPECT_FALSE(hd[1]);
+    EXPECT_TRUE (hd[0]);
+    EXPECT_TRUE (hd[1]);
+    EXPECT_FALSE(hd[2]);
+    EXPECT_FALSE(hd[3]);
+    EXPECT_TRUE (hd[4]);
+    EXPECT_TRUE (hd[5]);
+    EXPECT_FALSE(hd[6]);
+    EXPECT_FALSE(hd[7]);
+    EXPECT_TRUE (hd[8]);
+    EXPECT_TRUE (hd[9]);
+    EXPECT_FALSE(hd[10]);
+    EXPECT_FALSE(hd[11]);
 
     ret = cudaFree(d);
     ASSERT_EQ(cudaSuccess, ret);
