acf0342fec4456139953875d39ff103b77476677
Chris Kennelly <ckennelly@ckennelly.com>
2012-08-12 14:33:23 -0400

    Test sad with a single nonconstant operand.

--- src/tests/vtest_k_sad.cu
+++ src/tests/vtest_k_sad.cu
@@ -19,6 +19,7 @@
 #include <cuda.h>
 #include <gtest/gtest.h>
 #include <stdint.h>
+#include <valgrind/memcheck.h>
 
 __global__ void k_sad(const int * x, const int * y, const unsigned int * z,
         unsigned int * out, int32_t n) {
@@ -61,6 +62,82 @@ TEST(kSAD, SADConstant) {
     ASSERT_EQ(cudaSuccess, ret);
 
     EXPECT_EQ(4u, hout);
+}
+
+__global__ void k_sad_nonconstA(unsigned int * out, unsigned int A) {
+    unsigned int _out;
+    asm("sad.u32 %0, %1, 2, 3;\n" : "=r"(_out) : "r"(A));
+    *out = _out;
+}
+
+__global__ void k_sad_nonconstB(unsigned int * out, unsigned int B) {
+    unsigned int _out;
+    asm("sad.u32 %0, 1, %1, 3;\n" : "=r"(_out) : "r"(B));
+    *out = _out;
+}
+
+__global__ void k_sad_nonconstC(unsigned int * out, unsigned int C) {
+    unsigned int _out;
+    asm("sad.u32 %0, 1, 2, %1;\n" : "=r"(_out) : "r"(C));
+    *out = _out;
+}
+
+TEST(kSAD, SADPartialConstant) {
+    cudaError_t ret;
+    cudaStream_t stream;
+
+    const unsigned int A   = 1;
+          unsigned int Ain = A;
+    const unsigned int B   = 2;
+          unsigned int Bin = B;
+    const unsigned int C   = 3;
+          unsigned int Cin = C;
+
+    VALGRIND_MAKE_MEM_UNDEFINED(&Ain, sizeof(Ain));
+    VALGRIND_MAKE_MEM_UNDEFINED(&Bin, sizeof(Bin));
+    VALGRIND_MAKE_MEM_UNDEFINED(&Cin, sizeof(Cin));
+
+    unsigned int * out;
+    ret = cudaMalloc((void **) &out, 6 * sizeof(*out));
+    ASSERT_EQ(cudaSuccess, ret);
+
+    ret = cudaStreamCreate(&stream);
+    ASSERT_EQ(cudaSuccess, ret);
+
+    k_sad_nonconstA<<<1, 1, 0, stream>>>(out + 0, A  );
+    k_sad_nonconstA<<<1, 1, 0, stream>>>(out + 1, Ain);
+    k_sad_nonconstB<<<1, 1, 0, stream>>>(out + 2, B  );
+    k_sad_nonconstB<<<1, 1, 0, stream>>>(out + 3, Bin);
+    k_sad_nonconstC<<<1, 1, 0, stream>>>(out + 4, C  );
+    k_sad_nonconstC<<<1, 1, 0, stream>>>(out + 5, Cin);
+
+    ret = cudaStreamSynchronize(stream);
+    EXPECT_EQ(cudaSuccess, ret);
+
+    ret = cudaStreamDestroy(stream);
+    ASSERT_EQ(cudaSuccess, ret);
+
+    unsigned int hout[6];
+    ret = cudaMemcpy(&hout, out, sizeof(hout), cudaMemcpyDeviceToHost);
+    ASSERT_EQ(cudaSuccess, ret);
+
+    ret = cudaFree(out);
+    ASSERT_EQ(cudaSuccess, ret);
+
+    EXPECT_EQ(4u, hout[0]);
+    EXPECT_EQ(4u, hout[2]);
+    EXPECT_EQ(4u, hout[4]);
+
+    unsigned int vout[6];
+    const int vret = VALGRIND_GET_VBITS(hout, vout, sizeof(hout));
+    if (vret == 1) {
+        EXPECT_EQ( 0, vout[0]);
+        EXPECT_EQ(-1, vout[1]);
+        EXPECT_EQ( 0, vout[2]);
+        EXPECT_EQ(-1, vout[3]);
+        EXPECT_EQ( 0, vout[4]);
+        EXPECT_EQ(-1, vout[5]);
+    }
 }
 
 TEST(kSAD, SAD) {
